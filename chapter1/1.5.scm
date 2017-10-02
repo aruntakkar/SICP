e93247c71a2fb59c2b44f3c697b73650feac90a8
aruntakkar5@gmail.com <aruntakkar5@gmail.com>
2017-10-02 22:52:28 +0530

    "1.5 & 1.1.7"

--- chapter1/1.5.scm
+++ chapter1/1.5.scm
@@ -1,3 +1,10 @@
+;; Using applicative-order-evaluation, the evaluation of
+;; applicative-order-evaluation is used by lisp
+;; (test 0 (p)) never terminates, because (p) is infinitely expanded to itself
+;; (test 0 (p))
+;; (test 0 (p))
+;; ... so on
+
 (define (p) (p))
 
 (define (test x y)
@@ -6,3 +13,10 @@
       y))
 
 (test 0 (p))
+
+;; normal-order-evaluation is also known as fully expand then reduce
+;; Using normal-order-evaluation, the expression evaluates, step by step, to 0:
+;; (test 0 (p))
+;; (if (= 0 0) 0 (p))
+;; (if #t 0 (p))
+;; 0
